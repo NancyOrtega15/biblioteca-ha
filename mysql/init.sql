512182f2ed1db2d09dcf9aa20c687535ce528827
NancyOrtega15 <2330576@upt.edu.mx>
2026-04-11 21:18:06 -0600

    base de datos inicial

---
+++ mysql/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE biblioteca;
+
+USE biblioteca;
+
+CREATE TABLE libro (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(100)
+    Telefono VARCHAR(10)
+    Correo VARCHAR (30)
+    Rol VARCHAR (50)
+);
